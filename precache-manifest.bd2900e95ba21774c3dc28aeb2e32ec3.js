85ccb9aa58f7f667551a2a7b8800b9976927411e
pft\anders_chen <anders_chen@perfectcorp.com>
2019-10-14 15:01:17 +0800

    Updates

---
+++ precache-manifest.bd2900e95ba21774c3dc28aeb2e32ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dffec213fa1cc639b1eb4afd19b613",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7dbc78d79e75ad14980",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e346a61576c0d80a94e8",
+    "url": "./static/js/2.6e85ce2f.chunk.js"
+  },
+  {
+    "revision": "f7dbc78d79e75ad14980",
+    "url": "./static/js/main.856f2c4a.chunk.js"
+  },
+  {
+    "revision": "6698734341f9ec5fb24b",
+    "url": "./static/js/runtime-main.7ba6edab.js"
+  }
+]);
